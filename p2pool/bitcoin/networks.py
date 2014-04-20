2cb31d9a60666dd52e4b44e1f311cf9b3eaef04b
root <root@ubuntu.(none)>
2014-04-20 22:31:35 +0200

    testnet added

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -823,6 +823,31 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+
+    leafcoin_test=math.Object(
+        P2P_PREFIX='bbaaaaaa'.decode('hex'), #pchmessagestart
+        P2P_PORT=33813,
+        ADDRESS_VERSION=111, #pubkey_address
+        RPC_PORT=33812,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'leafcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 100000*100000000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=60, # s
+        SYMBOL='LEAF',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'LeafCoin') 
+		if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Leafcoin/') 
+		if platform.system() == 'Darwin' else os.path.expanduser('~/.leafcoin'), 'leafcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer2.leafco.in/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer2.leafco.in/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer2.leafco.in/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
+
     ekrona=math.Object(
         P2P_PREFIX='dcc1c104'.decode('hex'), #pchmessagestart
         P2P_PORT=9445,
--- p2pool/networks.py
+++ p2pool/networks.py
@@ -702,6 +702,27 @@ nets = dict(
 	 CHARITY_ADDRESS='abe20097fd1476fba24e6e87b2f31da2196cdb88'.decode('hex'),
         CHARITY_VALUE=50,
     ),
+
+    leafcoin_testnet=math.Object(
+        PARENT=networks.nets['leafcoin_testnet'],
+        SHARE_PERIOD=10, # seconds target spacing
+        CHAIN_LENGTH=12*60*60//10, # shares
+        REAL_CHAIN_LENGTH=12*60*60//10, # shares
+        TARGET_LOOKBEHIND=20, # shares coinbase maturity
+        SPREAD=50, # blocks
+        IDENTIFIER='fc756166636f696e'.decode('hex'),
+        PREFIX='fe636f696e6c656a'.decode('hex'),
+        P2P_PORT=8166,
+        MIN_TARGET=0,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9166,
+        BOOTSTRAP_ADDRS='p2pool-eu.gotgeeks.com p2pool-us.gotgeeks.com rav3n.dtdns.net doge.dtdns.net pool.hostv.pl p2pool.org p2pool.gotgeeks.com p2pool.dtdns.net solidpool.org taken.pl'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-alt',
+        VERSION_CHECK=lambda v: True,
+	 CHARITY_ADDRESS='4CDF68D16237578C9F5A1E8429622A833B1BCC76'.decode('hex'),
+        CHARITY_VALUE=50,
+    ),
     ekrona=math.Object(
         PARENT=networks.nets['ekrona'],
         SHARE_PERIOD=10, # seconds target spacing
